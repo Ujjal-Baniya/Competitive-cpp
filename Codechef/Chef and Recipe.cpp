aaa2118e4994e72a05640b79a856fee4c2fc4a61
Ujjal-Baniya <63017383+Ujjal-Baniya@users.noreply.github.com>
2020-09-16 19:35:49 +0530

    Create Chef and Recipe.cpp

---
+++ Codechef/Chef and Recipe.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    bool result = true;
+    int n, x, prev, arr[n];
+    int index[n];
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+}
