54f5f0c033b138aad6c911e79147fc2de097a7c7
UJJAL BANIYA <ujjalbaniya@gmail.com>
2020-10-01 09:41:42 +0545

    Update circle coloring.cpp

--- CodeForces/circle coloring.cpp
+++ CodeForces/circle coloring.cpp
@@ -18,37 +18,37 @@ int main() {
     	for(int i=0; i<n; i++) {
 			cin >> c[i];
 		}
-    	// first = a[0];
-    	// cout << first << " ";
-    	// int prev = first;
-    	// for(int i=1; i<n; i++){
-    	//     if(i==n-1){
-    	//         if(a[i]!= first && a[i] != prev){
-    	//            cout << a[i] << endl;
-    	//            break;
-    	//         } 
-    	//         else if(b[i]!= first && b[i] != prev){
-    	//            cout << b[i] << endl; 
-    	//            break;
-    	//         } 
-    	//         else if(c[i]!= first && c[i] != prev){
-    	//             cout << c[i] << endl;
-    	//             break;
-    	//         } 
-    	//     }
-    	//     if(a[i]!= prev){
-    	//         cout << a[i] <<" ";
-    	//         prev = a[i];
-    	//     } 
-    	//     else if(b[i]!= prev){
-    	//         cout << b[i] <<" ";
-    	//         prev = b[i];
-    	//     } 
-    	//     else if(c[i]!= prev){
-    	//         cout << c[i] <<" ";
-    	//         prev = c[i];
-    	//     } 
-    	// }
+    	first = a[0];
+    	cout << first << " ";
+    	int prev = first;
+    	for(int i=1; i<n; i++){
+    	    if(i==n-1){
+    	        if(a[i]!= first && a[i] != prev){
+    	           cout << a[i] << endl;
+    	           break;
+    	        } 
+    	        else if(b[i]!= first && b[i] != prev){
+    	           cout << b[i] << endl; 
+    	           break;
+    	        } 
+    	        else if(c[i]!= first && c[i] != prev){
+    	            cout << c[i] << endl;
+    	            break;
+    	        } 
+    	    }
+    	    if(a[i]!= prev){
+    	        cout << a[i] <<" ";
+    	        prev = a[i];
+    	    } 
+    	    else if(b[i]!= prev){
+    	        cout << b[i] <<" ";
+    	        prev = b[i];
+    	    } 
+    	    else if(c[i]!= prev){
+    	        cout << c[i] <<" ";
+    	        prev = c[i];
+    	    } 
+    	}
     }
 	return 0;
 }
