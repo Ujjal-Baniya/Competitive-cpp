baddd3b550375fe9dfd6e23dc3c7da6f2f74a8f2
UJJAL BANIYA <ujjalbaniya@gmail.com>
2020-10-05 21:58:23 +0545

    Create codu mockvita.cpp

---
+++ codu mockvita.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int m1, m2, r1, Mm, M1, M2, I;
+	cin >> m1 >> m2>> r1 >> Mm >> M1 >> M2 >> I;
+	cout << r1-M1-M2+I << " ";
+	cout << Mm - I << endl;
+	return 0;
+}
