e89026785cdf28d67da2b494716bdc4e5b1eafdc
UJJAL BANIYA <ujjalbaniya@gmail.com>
2021-06-23 11:25:06 +0530

    Update dp-possible.cpp

--- 2021 IKJOO/dp-possible.cpp
+++ 2021 IKJOO/dp-possible.cpp
@@ -1,29 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int countPossible(int size, int* arr){
-    int* a = new int[size+1];
+int countPossible(int s, int* arr){
+    int* a = new int[s+1];
     a[0] = 1;
     a[1] = 1;
-    for(int i=2; i<=size; i++){
+    for(int i=2; i<=s; i++){
         a[i] = a[i-1];
-        if((arr[i-2]*10 + arr[i-1])<=26){
+        if(arr[i-2]*10 + arr[i-1] <= 26){
             a[i] += a[i-2];
         }
     }
-    int ans = arr[size];
-    delete [] arr;
+    int ans = a[s];
+    delete [] a;
     return ans;
+}
+int repos(int s, int* arr){
+    if(s==1 || s==0) {
+        return 1;
+    }
+    int output = repos(s-1, arr);
+    if((arr[s-2]*10 + arr[s-1])<=26) {
+        output += repos(s-2, arr);
+    }
+    return output;
 }
 
 int main(){
-    int size;
-    cin >> size;
-    int* arr = new int[size+1];
-    for(int i=0; i<size; i++){
+    int s;
+    cin >> s;
+    int* arr = new int[s+1];
+    for(int i=0; i<s; i++){
         cin >> arr[i];
     }
-    int ans = countPossible(size, arr);
+    int ans = countPossible(s, arr);
     cout <<"Number of possible combination: " << ans;
     return 0;
 }
